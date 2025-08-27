37536ea565d27ad4d82cf24198b193dd346c8e5c
Lucky Nkolonzi <nkolonzilucky@gmail.com>
2025-08-27 13:56:49 +0200

    Separated the form to its own file so I can make it a client side component, then added a state to store the value to the checked input using the onChange event.

--- components/Filter.jsx
+++ components/Filter.jsx
@@ -1,39 +1,48 @@
-import React from 'react'
+"use client";
+import React from "react";
+import { useState } from "react";
 
 const filterOptions = [
-    {
-        id: "Vegan",
-        name: "filter",
-        value: "Vegan",
-    },
-    {
-        id: "gluten-free",
-        name: "filter",
-        value: "Gluten-Free",
-    },
-    {
-        id: "halal",
-        name: "filter",
-        value: "Halal",
-    },
-]
+  {
+    id: "Vegan",
+    name: "filter",
+    value: "Vegan",
+  },
+  {
+    id: "gluten-free",
+    name: "filter",
+    value: "Gluten-Free",
+  },
+  {
+    id: "halal",
+    name: "filter",
+    value: "Halal",
+  },
+];
 export default function Filter() {
+  const [selected, setSelected] = useState("Hello");
+  console.log(selected);
   return (
-     <form>
-          <h1 className="text-xl font-medium mb-4">Filter</h1>
-          
-          {filterOptions.map((filterOption) => {
-              return (
-                  <div className='flex gap-3'>
-                      <input name={filterOption.name}
-                          id={filterOption.id}
-                          value={filterOption.value}
-                          type='checkbox'
-                          />
-                          <label>{filterOption.value}</label>
-                  </div>
-              )
-          })}
+    <form>
+      <h1 className="text-xl font-medium mb-4">Filter</h1>
+
+      {filterOptions.map((filterOption) => {
+        return (
+          <div className="flex gap-3" key={filterOption.id}>
+            <input
+              name={filterOption.name}
+              id={filterOption.id}
+              value={filterOption.value}
+              onChange={(e) => {
+                setSelected(e.target.value);
+                console.log(e.target.value);
+              }}
+              type="checkbox"
+            />
+            <label>{filterOption.value}</label>
+          </div>
+        );
+      })}
     </form>
-  )
+  );
 }
